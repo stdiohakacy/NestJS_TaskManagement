e193daf1b9c8d8cd9c65c8716806a73381813d1a
Duy <huynhduytam2210@gmail.com>
2020-01-15 20:53:28 +0700

    CRUD tasks

---
+++ src/tasks/tasks.controller.ts
@@ -0,0 +1,36 @@
+import { TasksService } from './tasks.service';
+import { Controller, Get, Post, Body, Param, Delete, Patch } from '@nestjs/common';
+import { Task, TaskStatus } from './task.model';
+import { CreateTaskDTO } from './dto/create-task.dto';
+
+@Controller('tasks')
+export class TasksController {
+    constructor(private tasksService: TasksService){
+
+    }
+
+    @Get()
+    getAllTasks(): Task[] {
+        return this.tasksService.getAllTasks();
+    }
+
+    @Get('/:id')
+    getTaskById(@Param('id') id: string): Task{
+        return this.tasksService.getTaskById(id);
+    }
+
+    @Post()
+    createTask(@Body() createTaskDTO: CreateTaskDTO): Task {
+        return this.tasksService.createTask(createTaskDTO);
+    }
+
+    @Delete('/:id')
+    deleteTask(@Param('id')id: string): void{
+        this.tasksService.deleteTask(id);
+    }
+
+    @Patch('/:id/status')
+    updateTaskStatus(@Param('id') id: string, @Param('status') status: TaskStatus): Task {
+        return this.tasksService.updateTaskStatus(id, status);
+    }
+}
