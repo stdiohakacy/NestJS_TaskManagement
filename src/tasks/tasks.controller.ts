faed74e67e166339ce222d3bdc32ce68361067f1
Duy <huynhduytam2210@gmail.com>
2020-01-16 15:28:01 +0700

    Authorization Deleting a User's Task

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -33,8 +33,8 @@ export class TasksController {
     }
 
     @Delete('/:id')
-    deleteTask(@Param('id', ParseIntPipe) id: number): Promise<void> {
-        return this.tasksService.deleteTask(id);
+    deleteTask(@Param('id', ParseIntPipe) id: number, @GetUser() user: User): Promise<void> {
+        return this.tasksService.deleteTask(id, user);
     }
 
     @Patch('/:id/status')
