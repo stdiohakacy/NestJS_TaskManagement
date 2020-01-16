4d09255e5156a4294f4a6a4f79e0e59304dc07b5
Duy <huynhduytam2210@gmail.com>
2020-01-16 15:24:12 +0700

    Authorization Updating a User's Task Status

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -40,8 +40,8 @@ export class TasksController {
     @Patch('/:id/status')
     updateTaskStatus(
         @Param('id', ParseIntPipe) id: number,
-        @Body('status', TaskStatusValidationPipe)
-        status: TaskStatus): Promise<Task> {
-        return this.tasksService.updateTaskStatus(id, status);
+        @Body('status', TaskStatusValidationPipe) status: TaskStatus,
+        @GetUser() user: User): Promise<Task> {
+        return this.tasksService.updateTaskStatus(id, status, user);
     }
 }
